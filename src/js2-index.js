6a5b4f3b79dfd74a3df125789e71b10574a876bf
Alese4ka <alesun4ik@gmail.com>
2021-10-21 15:25:02 +0300

    feature/JS-2: -create calc function

--- src/js2-index.js
+++ src/js2-index.js
@@ -1,16 +1,76 @@
-let elem = document.querySelectorAll('.calculator__operand');
-let result = document.getElementById('calculator__result');
+//let operand = document.querySelectorAll('.calculator__operand');
+let wrapper = document.querySelector('.calculator__wrapper');
+let resultNumbers = document.getElementById('calculator__result');
+let clear = document.querySelector('.calculator__clear');
+let deleteLastN = document.querySelector('.calculator__delete');
+//let multiply = document.querySelector('.calculator__multiply');
+let equally = document.getElementById('calculator__equally');
+let btn = document.querySelectorAll('.btn');
 
-for (let i = elem.length - 1; i >= 0; i--) {
-    elem[i].addEventListener('click', calculateNumbers, false);
-  }
+clear.addEventListener('click', () => clearResult());
+equally.addEventListener('click', () => calc());
+deleteLastN.addEventListener('click', () => deleteLastNumber());
+
+
+
+let array = [];
+wrapper.onclick = function(event) {
   
-  /*
-[...calculate].forEach((f) => f.onclick = () => calculateNumbers());
+  let number = event.target.closest('button'); // (1)
+ 
+  if (!number) return; // (2)
+  if (!wrapper.contains(number)) return; // (3)
 
-      */
-function calculateNumbers(){
-
-   result.value = this.innerHTML;
-
+  if (resultNumbers.value == 0){
+    if(number.value !== '*' && number.value !== '/' && number.value !== '+' && number.value !== '-'){
+      resultNumbers.value = number.value;
+      array.push(Number(resultNumbers.value))
+    }
   }
+  else {
+    resultNumbers.value += number.value;
+    array.push(number.value)
+  }
+console.log(array)
+
+};
+
+function clearResult() {
+  resultNumbers.value = 0;
+  array = []
+}
+
+function deleteLastNumber(){
+  array.pop();
+  resultNumbers.value = array;//убрать запятую
+}
+
+function calc() {
+    let w;
+    let a = Number(array[0]);
+    let c = array[1];
+    let b = Number(array[2]);
+      switch (c){
+        case '+':
+          w = a+b;
+          break;
+        case '-':
+           w = a-b;
+           break;
+        case '*':
+          w = a*b;
+          break;
+        case '/':
+          if(b != 0) {
+            w = a/b;  
+          }
+          else{
+            w = 'error';
+            return w;
+          }
+          break;
+        }
+        resultNumbers.value = w;
+        array = [w]
+  }
+
