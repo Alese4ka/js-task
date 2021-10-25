931c7a0a4b3df722299c22bfea11af47dbf376fb
Alese4ka <alesun4ik@gmail.com>
2021-10-26 01:07:09 +0300

    feature/JS-2: -create onkeyup function

--- src/js2-index.js
+++ src/js2-index.js
@@ -3,100 +3,99 @@ let resultNumbers = document.getElementById('calculator__result');
 let clear = document.querySelector('.calculator__clear');
 let deleteLastN = document.querySelector('.calculator__delete');
 let equally = document.getElementById('calculator__equally');
-let btn = document.querySelectorAll('.btn');
 
 clear.addEventListener('click', () => clearResult());
 equally.addEventListener('click', () => calcNumber());
 deleteLastN.addEventListener('click', () => deleteLastNumber());
 
-let a = '';
+let allResult = '';
 
 wrapper.onclick = function(event) {
   let number = event.target.closest('button'); 
   if (!number) return; 
   if (!wrapper.contains(number)) return; 
   resultNumbers.value = '';
-  resultNumbers.value = a + number.value;
-  a += number.value;
+  resultNumbers.value = allResult + number.value;
+  allResult += number.value;
 }
 
 function clearResult() {
   resultNumbers.value = '';
-  a = '';
+  allResult = '';
 }
 
-function deleteLastNumber(){
-  let c = resultNumbers.value.substring(0, resultNumbers.value.length - 1);
-  resultNumbers.value =  c
+function deleteLastNumber() {
+  let clearResult = resultNumbers.value.substr(0, resultNumbers.value.length - 1);
+  resultNumbers.value =  clearResult;
+  allResult = '';
 }
 
 function calcNumber() {
-  let w = eval(a);
-  resultNumbers.value = w;
-  a = w;
+  let lastResult = eval(allResult);
+  resultNumbers.value = lastResult;
+  allResult = lastResult;
 }
 
  wrapper.onkeyup = e => {
   if(e.key == "0" || e.key == "Num0"){
-    type_to(0);
+    typeTo(0);
   } else if(e.key == "1" || e.key == "Num1"){
-    type_to(1);
+    typeTo(1);
   } else if (e.key == "2" || e.key == "Num2") {
-    type_to(2);
+    typeTo(2);
   } else if (e.key == "3" || e.key == "Num3") {
-    type_to(3);
+    typeTo(3);
   } else if (e.key == "4" || e.key == "Num4") {
-    type_to(4);
+    typeTo(4);
   } else if (e.key == "5" || e.key == "Num5") {
-    type_to(5);
+    typeTo(5);
   } else if (e.key == "6" || e.key == "Num6") {
-    type_to(6);
+    typeTo(6);
   } else if (e.key == "7" || e.key == "Num7") {
-    type_to(7);
+    typeTo(7);
   } else if (e.key == "8" || e.key == "Num8") {
-    type_to(8);
+    typeTo(8);
   } else if (e.key == "9" || e.key == "Num9") {
-    type_to(9);
+    typeTo(9);
   } else if (e.key == "+" || e.key == "Num+") {
-    type_to("+");
+    typeTo("+");
   } else if (e.key == "-" || e.key == "Num-") {
-    type_to("-");
+    typeTo("-");
   } else if (e.key == "*" || e.key == "Num*") {
-    type_to("X");
+    typeTo("*");
   } else if (e.key == "/" || e.key == "Num/") {
-    type_to("/");
+    typeTo("/");
   } else if (e.key == "." || e.key == "Num.") {
-    type_to(".");
+    typeTo(".");
   } else if(e.key == "Enter" || e.key == "NumEnter"){
-    set_ans();
+    setAns();
   } else if (e.key == "Backspace") {
-    erase_screen();
+    eraseScreen();
   } else if (e.key == "Delete" || e.key == "Escape") {
-    screen_reset();
+    screenReset();
   }
 }
 
-let type_to = () =>{
-  let w = ''
+let typeTo = () =>{
   if (resultNumbers.value == ""){
-    resultNumbers.value = w;
+    resultNumbers.value = allResult;
   } 
   else if (resultNumbers.value.length <= 23){
-    resultNumbers.value = resultNumbers.value + w;
+    resultNumbers.value = resultNumbers.value + allResult;
   }
 }
 
-let set_ans = () => {
-  let write = resultNumbers.value;
-  let res = eval(write);
-  resultNumbers.value = res;
+let setAns = () => {
+  let writeNumber = resultNumbers.value;
+  let lastResult = eval(writeNumber);
+  resultNumbers.value = lastResult;
 }
 
-let screen_reset = () =>{
+let screenReset = () =>{
   resultNumbers.value = "";
 }
 
-let erase_screen = () =>{
-  let init = resultNumbers.value.substr(0, resultNumbers.value.length - 1);
-  resultNumbers.value = init;
+let eraseScreen = () =>{
+  let clearResult = resultNumbers.value.substr(0, resultNumbers.value.length - 1);
+  resultNumbers.value = clearResult;
 }
