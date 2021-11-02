e0191218d47bfed220413a15c2d0ece755f8bd02
Alese4ka <alesun4ik@gmail.com>
2021-11-03 00:25:58 +0300

    feature/JS-3: -create timer function

---
+++ src/assets/scripts/pomodoro.js
@@ -0,0 +1,53 @@
+const timeToWork = document.querySelector('.pomodoro__clock__timer-minutes');
+const timeToBreak = document.querySelector('.pomodoro__clock__break-minutes');
+const startBtn = document.querySelector('.pomodoro__buttons-start');
+const stopBtn = document.querySelector('.pomodoro__buttons-stop');
+const timerClock = document.querySelector('.pomodoro__time-clock');
+
+//Добавить паузу
+//Добавить сигнал
+//Сделать новый таймер внутри
+
+startBtn.addEventListener('click', function() {
+  timeMinute = parseInt(timeToWork.value) * 60;
+})
+
+function timerBreak(){
+  setInterval(function () {
+  timeMinuteBreak = parseInt(timeToBreak.value) * 60;
+  seconds = Math.floor(timeMinuteBreak%60);
+  minutes = Math.floor(timeMinuteBreak/60%60);
+  if (timeMinuteBreak <= 0) {
+      clearInterval(timerBreak);
+  } else if (seconds < 10) {
+    let strTimer = `${minutes}:0${seconds}`;
+    timerClock.innerHTML = strTimer;
+  }
+  else {
+      let strTimer = `${minutes}:${seconds}`;
+      timerClock.innerHTML = strTimer;
+  }
+  --timeMinuteBreak;
+}, 1000);
+}
+
+timer = setInterval(function () {
+  seconds = Math.floor(timeMinute%60);
+  minutes = Math.floor(timeMinute/60%60);
+  if (timeMinute <= 0) {
+    timerBreak();
+  } else if (seconds < 10) {
+    let strTimer = `${minutes}:0${seconds}`;
+    timerClock.innerHTML = strTimer;
+  }
+  else {
+      let strTimer = `${minutes}:${seconds}`;
+      timerClock.innerHTML = strTimer;
+  }
+  --timeMinute; 
+}, 1000)
+
+stopBtn.addEventListener('click', function() {
+  clearInterval(timer);
+  timerClock.innerHTML = `${timeToWork.value}:00`;
+})
