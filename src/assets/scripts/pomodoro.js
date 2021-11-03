4acb0c0152339a46791bd074a14da696965758d1
Alese4ka <alesun4ik@gmail.com>
2021-11-03 15:54:21 +0300

    feature/JS-3: -add pause and resume function

--- src/assets/scripts/pomodoro.js
+++ src/assets/scripts/pomodoro.js
@@ -1,11 +1,9 @@
 const timeToWork = document.querySelector('.pomodoro__clock__timer-minutes');
 const timeToBreak = document.querySelector('.pomodoro__clock__break-minutes');
 const startBtn = document.querySelector('.pomodoro__buttons-start');
-const stopBtn = document.querySelector('.pomodoro__buttons-stop');
+const pauseBtn = document.querySelector('.pomodoro__buttons-pause');
+const resumeBtn = document.querySelector('.pomodoro__buttons-resume');
 const timerClock = document.querySelector('.pomodoro__time-clock');
-
-//Добавить паузу -
-//Добавить стоп +-
 
 startBtn.addEventListener('click', function() {
   timeMinute = parseInt(timeToWork.value) * 60;
@@ -79,7 +77,74 @@ timer = setInterval(function () {
   --timeMinute; 
 }, 1000);
 
-stopBtn.addEventListener('click', function() {
+pauseBtn.addEventListener('click', function() {
   clearInterval(timer);
-  timerClock.innerHTML = `${timeToWork.value}:00`;
+})
+
+resumeBtn.addEventListener('click', function() {
+  timer = setInterval(function () {
+    seconds = Math.floor(timeMinute%60);
+    minutes = Math.floor(timeMinute/60%60);
+    if (timeMinute <= 0) {
+      clearInterval(timer);
+      sound.play();
+      timeMinuteBreak = parseInt(timeToBreak.value) * 60;
+      timer = setInterval(function () {
+        seconds = Math.floor(timeMinuteBreak%60);
+        minutes = Math.floor(timeMinuteBreak/60%60);
+        if (timeMinuteBreak <= 0) {
+          clearInterval(timer);
+          sound.play();
+          timer = setInterval(function () {
+            seconds = Math.floor(timeMinute%60);
+            minutes = Math.floor(timeMinute/60%60);
+            if (timeMinute <= 0) {
+              clearInterval(timer);
+              sound.play();
+              timeMinuteBreak = parseInt(timeToBreak.value) * 60;
+              setInterval(function () {
+                seconds = Math.floor(timeMinuteBreak%60);
+                minutes = Math.floor(timeMinuteBreak/60%60);
+                if (timeMinuteBreak <= 0) {
+                  timer;
+                } else if (seconds < 10) {
+                  let strTimer = `${minutes}:0${seconds}`;
+                  timerClock.innerHTML = strTimer;
+                }
+                else {
+                    let strTimer = `${minutes}:${seconds}`;
+                    timerClock.innerHTML = strTimer;
+                }
+                --timeMinuteBreak;
+              }, 1000);
+            } else if (seconds < 10) {
+              let strTimer = `${minutes}:0${seconds}`;
+              timerClock.innerHTML = strTimer;
+            }
+            else {
+              let strTimer = `${minutes}:${seconds}`;
+              timerClock.innerHTML = strTimer;
+            }
+            --timeMinute; 
+          }, 1000)
+        } else if (seconds < 10) {
+          let strTimer = `${minutes}:0${seconds}`;
+          timerClock.innerHTML = strTimer;
+        }
+        else {
+            let strTimer = `${minutes}:${seconds}`;
+            timerClock.innerHTML = strTimer;
+        }
+        --timeMinuteBreak;
+      }, 1000);
+    } else if (seconds < 10) {
+      let strTimer = `${minutes}:0${seconds}`;
+      timerClock.innerHTML = strTimer;
+    }
+    else {
+      let strTimer = `${minutes}:${seconds}`;
+      timerClock.innerHTML = strTimer;
+    }
+    --timeMinute; 
+  }, 1000);
 })
