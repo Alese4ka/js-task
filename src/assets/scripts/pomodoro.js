1fccf4b1e47230ba396d19197bee009374470f79
Alese4ka <alesun4ik@gmail.com>
2021-11-04 15:34:24 +0300

    feature/JS-3: -change mistakes

--- src/assets/scripts/pomodoro.js
+++ src/assets/scripts/pomodoro.js
@@ -1,8 +1,8 @@
 const timeToWork = document.querySelector('.pomodoro__clock__timer-minutes');
 const timeToBreak = document.querySelector('.pomodoro__clock__break-minutes');
 const startBtn = document.querySelector('.pomodoro__buttons-start');
-const pauseBtn = document.querySelector('.pomodoro__buttons-pause');
-const resumeBtn = document.querySelector('.pomodoro__buttons-resume');
+const stopBtn = document.querySelector('.pomodoro__buttons-stop');
+const deleteBtn = document.querySelector('.pomodoro__buttons-delete');
 const timerClock = document.querySelector('.pomodoro__time-clock');
 
 startBtn.addEventListener('click', function() {
@@ -77,74 +77,15 @@ timer = setInterval(function () {
   --timeMinute; 
 }, 1000);
 
-pauseBtn.addEventListener('click', function() {
+stopBtn.addEventListener('click', function() {
   clearInterval(timer);
 })
 
-resumeBtn.addEventListener('click', function() {
-  timer = setInterval(function () {
-    seconds = Math.floor(timeMinute%60);
-    minutes = Math.floor(timeMinute/60%60);
-    if (timeMinute <= 0) {
-      clearInterval(timer);
-      sound.play();
-      timeMinuteBreak = parseInt(timeToBreak.value) * 60;
-      timer = setInterval(function () {
-        seconds = Math.floor(timeMinuteBreak%60);
-        minutes = Math.floor(timeMinuteBreak/60%60);
-        if (timeMinuteBreak <= 0) {
-          clearInterval(timer);
-          sound.play();
-          timer = setInterval(function () {
-            seconds = Math.floor(timeMinute%60);
-            minutes = Math.floor(timeMinute/60%60);
-            if (timeMinute <= 0) {
-              clearInterval(timer);
-              sound.play();
-              timeMinuteBreak = parseInt(timeToBreak.value) * 60;
-              setInterval(function () {
-                seconds = Math.floor(timeMinuteBreak%60);
-                minutes = Math.floor(timeMinuteBreak/60%60);
-                if (timeMinuteBreak <= 0) {
-                  timer;
-                } else if (seconds < 10) {
-                  let strTimer = `${minutes}:0${seconds}`;
-                  timerClock.innerHTML = strTimer;
-                }
-                else {
-                    let strTimer = `${minutes}:${seconds}`;
-                    timerClock.innerHTML = strTimer;
-                }
-                --timeMinuteBreak;
-              }, 1000);
-            } else if (seconds < 10) {
-              let strTimer = `${minutes}:0${seconds}`;
-              timerClock.innerHTML = strTimer;
-            }
-            else {
-              let strTimer = `${minutes}:${seconds}`;
-              timerClock.innerHTML = strTimer;
-            }
-            --timeMinute; 
-          }, 1000)
-        } else if (seconds < 10) {
-          let strTimer = `${minutes}:0${seconds}`;
-          timerClock.innerHTML = strTimer;
-        }
-        else {
-            let strTimer = `${minutes}:${seconds}`;
-            timerClock.innerHTML = strTimer;
-        }
-        --timeMinuteBreak;
-      }, 1000);
-    } else if (seconds < 10) {
-      let strTimer = `${minutes}:0${seconds}`;
-      timerClock.innerHTML = strTimer;
-    }
-    else {
-      let strTimer = `${minutes}:${seconds}`;
-      timerClock.innerHTML = strTimer;
-    }
-    --timeMinute; 
-  }, 1000);
+deleteBtn.addEventListener('click', function() {
+  clearInterval(timer);
+  timerClock.innerHTML = `${timeToWork.value}:00`;
 })
+
+
+
+
