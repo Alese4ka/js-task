a3871437356f3df25999752094ff7c0b48f63938
Alese4ka <alesun4ik@gmail.com>
2021-11-05 14:37:15 +0300

    feature/JS-3: -change mistakes

--- src/assets/scripts/pomodoro.js
+++ src/assets/scripts/pomodoro.js
@@ -4,88 +4,43 @@ const startBtn = document.querySelector('.pomodoro__buttons-start');
 const stopBtn = document.querySelector('.pomodoro__buttons-stop');
 const deleteBtn = document.querySelector('.pomodoro__buttons-delete');
 const timerClock = document.querySelector('.pomodoro__time-clock');
+const sound = document.getElementById('sound');
 
-startBtn.addEventListener('click', function() {
-  timeMinute = parseInt(timeToWork.value) * 60;
-})
+let timeMinute;
+let timer;
 
-const sound = document.getElementById("ado");
+timeMinute = parseInt(timeToWork.value) * 60;
 
-timer = setInterval(function () {
-  seconds = Math.floor(timeMinute%60);
-  minutes = Math.floor(timeMinute/60%60);
-  if (timeMinute <= 0) {
-    clearInterval(timer);
-    sound.play();
-    timeMinuteBreak = parseInt(timeToBreak.value) * 60;
-    timer = setInterval(function () {
-      seconds = Math.floor(timeMinuteBreak%60);
-      minutes = Math.floor(timeMinuteBreak/60%60);
-      if (timeMinuteBreak <= 0) {
-        clearInterval(timer);
-        sound.play();
-        timer = setInterval(function () {
-          seconds = Math.floor(timeMinute%60);
-          minutes = Math.floor(timeMinute/60%60);
-          if (timeMinute <= 0) {
-            clearInterval(timer);
-            sound.play();
-            timeMinuteBreak = parseInt(timeToBreak.value) * 60;
-            setInterval(function () {
-              seconds = Math.floor(timeMinuteBreak%60);
-              minutes = Math.floor(timeMinuteBreak/60%60);
-              if (timeMinuteBreak <= 0) {
-                timer;
-              } else if (seconds < 10) {
-                let strTimer = `${minutes}:0${seconds}`;
-                timerClock.innerHTML = strTimer;
-              }
-              else {
-                  let strTimer = `${minutes}:${seconds}`;
-                  timerClock.innerHTML = strTimer;
-              }
-              --timeMinuteBreak;
-            }, 1000);
-          } else if (seconds < 10) {
-            let strTimer = `${minutes}:0${seconds}`;
-            timerClock.innerHTML = strTimer;
-          }
-          else {
-            let strTimer = `${minutes}:${seconds}`;
-            timerClock.innerHTML = strTimer;
-          }
-          --timeMinute; 
-        }, 1000)
-      } else if (seconds < 10) {
-        let strTimer = `${minutes}:0${seconds}`;
-        timerClock.innerHTML = strTimer;
-      }
-      else {
-          let strTimer = `${minutes}:${seconds}`;
-          timerClock.innerHTML = strTimer;
-      }
-      --timeMinuteBreak;
-    }, 1000);
-  } else if (seconds < 10) {
-    let strTimer = `${minutes}:0${seconds}`;
-    timerClock.innerHTML = strTimer;
-  }
-  else {
-    let strTimer = `${minutes}:${seconds}`;
-    timerClock.innerHTML = strTimer;
-  }
-  --timeMinute; 
-}, 1000);
+function startTimer(timeMinute){
+  timer = setInterval(function( ){
+    seconds = Math.floor(timeMinute%60);
+    minutes = Math.floor(timeMinute/60%60);
+    if (timeMinute <= 0) {
+      clearInterval(timer);
+      sound.play();
+      timeMinute = parseInt(timeToBreak.value) * 60;
+      startTimer(timeMinute);
+    } else if (seconds < 10) {
+      let strTimer = `${minutes}:0${seconds}`;
+      timerClock.innerHTML = strTimer;
+    }
+    else {
+      let strTimer = `${minutes}:${seconds}`;
+      timerClock.innerHTML = strTimer;
+    }
+    --timeMinute; 
+  }, 1000);
+}
 
 stopBtn.addEventListener('click', function() {
   clearInterval(timer);
+})
+
+startBtn.addEventListener('click', function() {
+  startTimer(timeMinute);
 })
 
 deleteBtn.addEventListener('click', function() {
   clearInterval(timer);
   timerClock.innerHTML = `${timeToWork.value}:00`;
 })
-
-
-
-
