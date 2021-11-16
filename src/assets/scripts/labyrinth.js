d62d1c6d6e6c0155abe75bbc345379d52e30251f
Alese4ka <alesun4ik@gmail.com>
2021-11-16 17:10:49 +0300

    feature/JS-4: -add condition about black cells

--- src/assets/scripts/labyrinth.js
+++ src/assets/scripts/labyrinth.js
@@ -37,91 +37,114 @@ function generateCellFinish() {
 
 let coordinatesSmile = generateCellSmile();
 let coordinatesFinish = generateCellFinish();
-let smile = [document.querySelector('[posX = "' + coordinatesSmile[0] + '"][posY = "' + coordinatesSmile[1] + '"]')];
-let finish = [document.querySelector('[posX = "' + coordinatesFinish[0] + '"][posY = "' + coordinatesFinish[1] + '"]')];
+let smile = document.querySelector('[posX = "' + coordinatesSmile[0] + '"][posY = "' + coordinatesSmile[1] + '"]');
+let finish = document.querySelector('[posX = "' + coordinatesFinish[0] + '"][posY = "' + coordinatesFinish[1] + '"]');
 
-while (finish[0].classList.contains('smile')) {
+while (finish.classList.contains('smile')) {
   let coordinatesFinish = generateCellFinish();
-  finish = [document.querySelector('[posX = "' + coordinatesFinish[0] + '"][posY = "' + coordinatesFinish[1] + '"]')];
+  finish = document.querySelector('[posX = "' + coordinatesFinish[0] + '"][posY = "' + coordinatesFinish[1] + '"]');
 }
 
-smile[0].classList.add('smile');
-finish[0].classList.add('finish');
-cell[1].classList.toggle('black-cell') 
+c();
+function c(){
+  smile.classList.add('smile');
+  finish.classList.add('finish');
+  let rand = Math.round(Math.random() * (100-1) + 1);
+  cell[rand].classList.add('black-cell');
+}
+
 
 document.addEventListener('keydown', (e) => {
   e.preventDefault();
   if(e.code === 'ArrowRight') {
-    smile[0].classList.remove('smile');
-    smileMove = [smile[0].getAttribute('posX'), smile[0].getAttribute('posY')];
-    smile = [document.querySelector('[posX = "' + (+smileMove[0] + 1) + '"][posY = "' + smileMove[1] + '"]')];
-    smile[0].classList.add('smile');
-    if (smile[0].getAttribute('posX') == finish[0].getAttribute('posX') && smile[0].getAttribute('posY') == finish[0].getAttribute('posY')) {
-      alert('Вы выиграли');
-      smile[0].classList.remove('smile');
-      smile = [document.querySelector('[posX = "10"][posY = "10"]')];
-      smile[0].classList.add('smile');
-      levelUp();
+    smile.classList.remove('smile');
+    smileMove = [smile.getAttribute('posX'), smile.getAttribute('posY')];
+    smile = document.querySelector('[posX = "' + (+smileMove[0] + 1) + '"][posY = "' + smileMove[1] + '"]');
+    smile.classList.add('smile');
+    if (smile.classList.contains('black-cell')) {
+      alert('Вы проиграли');
+      newGame();
     }
-    else if (smile[0].getAttribute('posX') == cell[0].getAttribute('10')) {
-      console.log('jjjj')
+    else if (smile.getAttribute('posX') == finish.getAttribute('posX') && smile.getAttribute('posY') == finish.getAttribute('posY')) {
+      alert('Вы выиграли');
+      smile.classList.remove('smile');
+      smile = document.querySelector('[posX = "10"][posY = "10"]');
+      smile.classList.add('smile');
+      levelUp();
     }
   }
   else if (e.code === 'ArrowLeft') {
-    smile[0].classList.remove('smile');
-    smileMove = [smile[0].getAttribute('posX'), smile[0].getAttribute('posY')];
-    smile = [document.querySelector('[posX = "' + (+smileMove[0] - 1) + '"][posY = "' + smileMove[1] + '"]')];
-    smile[0].classList.add('smile');
-    if (smile[0].getAttribute('posX') == finish[0].getAttribute('posX') && smile[0].getAttribute('posY') == finish[0].getAttribute('posY')) {
-      alert('Вы выиграли')
-      smile[0].classList.remove('smile');
-      smile = [document.querySelector('[posX = "10"][posY = "10"]')];
-      smile[0].classList.add('smile');
-      levelUp();
+    smile.classList.remove('smile');
+    smileMove = [smile.getAttribute('posX'), smile.getAttribute('posY')];
+    smile = document.querySelector('[posX = "' + (+smileMove[0] - 1) + '"][posY = "' + smileMove[1] + '"]');
+    smile.classList.add('smile');
+    if (smile.classList.contains('black-cell')) {
+      alert('Вы проиграли');
+      newGame();
     }
-    else if (smile[0].getAttribute('posX') == 10) {
-      alert('Вы проиграли')
+    else if (smile.getAttribute('posX') == finish.getAttribute('posX') && smile.getAttribute('posY') == finish.getAttribute('posY')) {
+      alert('Вы выиграли')
+      smile.classList.remove('smile');
+      smile = document.querySelector('[posX = "10"][posY = "10"]');
+      smile.classList.add('smile');
+      levelUp();
     }
   }
   else if (e.code === 'ArrowUp') {
-    smile[0].classList.remove('smile');
-    smileMove = [smile[0].getAttribute('posX'), smile[0].getAttribute('posY')];
-    smile = [document.querySelector('[posX = "' + smileMove[0] + '"][posY = "' + (+smileMove[1] + 1) + '"]')];
-    smile[0].classList.add('smile');
-    if (smile[0].getAttribute('posX') == finish[0].getAttribute('posX') && smile[0].getAttribute('posY') == finish[0].getAttribute('posY')) {
+    smile.classList.remove('smile');
+    smileMove = [smile.getAttribute('posX'), smile.getAttribute('posY')];
+    smile = document.querySelector('[posX = "' + smileMove[0] + '"][posY = "' + (+smileMove[1] + 1) + '"]');
+    smile.classList.add('smile');
+    if (smile.classList.contains('black-cell')) {
+      alert('Вы проиграли');
+      newGame();
+    }
+    else if (smile.getAttribute('posX') == finish.getAttribute('posX') && smile.getAttribute('posY') == finish.getAttribute('posY')) {
       alert('Вы выиграли');
-      smile[0].classList.remove('smile');
-      smile = [document.querySelector('[posX = "10"][posY = "10"]')];
-      smile[0].classList.add('smile');
+      smile.classList.remove('smile');
+      smile = document.querySelector('[posX = "10"][posY = "10"]');
+      smile.classList.add('smile');
       levelUp();
     }
   }
   else if (e.code === 'ArrowDown') {
-    smile[0].classList.remove('smile');
-    smileMove = [smile[0].getAttribute('posX'), smile[0].getAttribute('posY')];
-    smile = [document.querySelector('[posX = "' + smileMove[0] + '"][posY = "' + (+smileMove[1] - 1) + '"]')];
-    smile[0].classList.add('smile');
-    if (smile[0].getAttribute('posX') == finish[0].getAttribute('posX') && smile[0].getAttribute('posY') == finish[0].getAttribute('posY')) {
+    smile.classList.remove('smile');
+    smileMove = [smile.getAttribute('posX'), smile.getAttribute('posY')];
+    smile = document.querySelector('[posX = "' + smileMove[0] + '"][posY = "' + (+smileMove[1] - 1) + '"]');
+    smile.classList.add('smile');
+    if (smile.classList.contains('black-cell')) {
+      alert('Вы проиграли');
+      newGame();
+    }
+    else if (smile.getAttribute('posX') == finish.getAttribute('posX') && smile.getAttribute('posY') == finish.getAttribute('posY')) {
       alert('Вы выиграли');
-      smile[0].classList.remove('smile');
-      smile = [document.querySelector('[posX = "10"][posY = "10"]')];
-      smile[0].classList.add('smile');
+      smile.classList.remove('smile');
+      smile = document.querySelector('[posX = "10"][posY = "10"]');
+      smile.classList.add('smile');
       levelUp();
     }
   } 
 })
 
+function newGame() {
+  smile.classList.remove('smile');
+  smile = document.querySelector('[posX = "10"][posY = "10"]');
+  smile.classList.add('smile');
+  finish.classList.remove('finish');
+  finish = document.querySelector('[posX = "5"][posY = "5"]');
+  finish.classList.add('finish');
+  cell.classList.remove('black-cell');//как быть с рандомными черными клетками?
+  countLevel.innerHTML = ` 1`;
+}
+
 function levelUp() {
   let rand = Math.round(Math.random() * (100-1) + 1);
-  if (rand == 10){
-    let rand = Math.round(Math.random() * (100-1) + 1);
-    cell[rand].classList.add('black-cell');
-  }
   cell[rand].classList.add('black-cell');
   let a = parseInt(1);
   let b = parseInt(countLevel.innerHTML);
   countLevel.innerHTML = ` ${a + b}`;
 } 
 
-// подумать как сделать если смайл сталкивается с границей и с черным чтобы был конец игры
-// сделать так чтобы черные ячейки не могли появляться в том месте где у же есть черный квадрат
+// подумать как сделать если смайл сталкивается с границей 
+// после того как столкнулся с черным -  обновить, обновляет но что делать с черными клетками
+// сделать так чтобы черные ячейки не могли появляться в том месте, где у же есть черный квадрат+-
