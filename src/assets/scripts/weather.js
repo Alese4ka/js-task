e84e4deae5a51e0e1d412dd993142e4512e55d0c
Alese4ka <alesun4ik@gmail.com>
2021-11-30 15:25:55 +0300

    feature/JS-5: -get geolocation

--- src/assets/scripts/weather.js
+++ src/assets/scripts/weather.js
@@ -1,12 +1,28 @@
-fetch('http://api.openweathermap.org/data/2.5/weather?id=2172797&appid=ea7f2ec7419b33064accc22aac5169a6')
-    .then(function (resp) {return resp.json()})
-    .then(function (data) {
-        console.log(data)
-        document.querySelector('.weather__city').innerHTML = data.name;
-        document.querySelector('.weather__forecast').innerHTML = Math.round(data.main.temp - 273) + '&deg;';
-        document.querySelector('.weather__desc').textContent = data.weather[0]['description'];
-        document.querySelector('.weather__icon').innerHTML = `<img src="https://openweathermap.org/img/wn/${data.weather[0]['icon']}@2x.png">`;
-    })
-    .catch(function () {
-        //Обрабатываем ошибки
-    });
+let geoCity = document.querySelector('#result');
+
+ymaps.ready(init);
+
+function init() {
+    ymaps.geolocation.get({
+    provider:'yandex'
+})
+.then(function(result){
+    let components = result.geoObjects.get(0).properties.get('metaDataProperty').GeocoderMetaData.Address.Components;
+    geoCity.innerHTML = `${components[2].name}`;
+})
+.catch(function () {
+    //Обрабатываем ошибки
+});
+}
+
+fetch('http://api.openweathermap.org/data/2.5/weather?q=Minsk&appid=ea7f2ec7419b33064accc22aac5169a6')
+.then(function (resp) {return resp.json()})
+.then(function (data) {
+    document.querySelector('.weather__city').innerHTML = data.name;
+    document.querySelector('.weather__temp').innerHTML = Math.round(data.main.temp - 273) + '&deg;';
+    document.querySelector('.weather__info').textContent = data.weather[0]['description'];
+    document.querySelector('.weather__icon').innerHTML = `<img src="https://openweathermap.org/img/wn/${data.weather[0]['icon']}@2x.png">`;
+})
+.catch(function () {
+    //Обрабатываем ошибки
+});
