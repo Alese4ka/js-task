43d7e2665a2a342fabe938dbbc2bca4f597ee9a1
Alese4ka <alesun4ik@gmail.com>
2021-12-02 13:44:47 +0300

    feature/JS-5: -create changeTemp

--- src/assets/scripts/weather.js
+++ src/assets/scripts/weather.js
@@ -2,6 +2,7 @@ let wrapper = document.querySelector('.weather');
 let otherCity = document.querySelector('.weather__other-city');
 let switchTemp = document.querySelector('.weather__switch');
 let weatherCity = document.querySelector('.weather__city');
+let weatherTemp = document.querySelector('.weather__temp');
 let city;
 
 ymaps.ready(init);
@@ -26,7 +27,7 @@ wrapper.onkeyup = e => {
         .then(function (resp) {return resp.json()})
         .then(function (data) {
             document.querySelector('.weather__city').innerHTML = data.name;
-            document.querySelector('.weather__temp').innerHTML = Math.round(data.main.temp - 273) + ' C&deg;';
+            document.querySelector('.weather__temp').innerHTML = Math.round(data.main.temp - 273);
             document.querySelector('.weather__info').textContent = data.weather[0]['description'];
             document.querySelector('.weather__icon').innerHTML = `<img src="https://openweathermap.org/img/wn/${data.weather[0]['icon']}@2x.png">`;
             otherCity.value = '';
@@ -43,7 +44,7 @@ function getWeather() {
     .then(function (resp) {return resp.json()})
     .then(function (data) {
         document.querySelector('.weather__city').innerHTML = data.name;
-        document.querySelector('.weather__temp').innerHTML = Math.round(data.main.temp - 273) + ' C&deg;';
+        document.querySelector('.weather__temp').innerHTML = Math.round(data.main.temp - 273);
         document.querySelector('.weather__info').textContent = data.weather[0]['description'];
         document.querySelector('.weather__icon').innerHTML = `<img src="https://openweathermap.org/img/wn/${data.weather[0]['icon']}@2x.png">`;
     })
@@ -57,25 +58,13 @@ let switchStatus = true;
 
 function changeTemp() {
     if(switchStatus == true){
-        fetch(`http://api.openweathermap.org/data/2.5/weather?q=${weatherCity.innerHTML}&appid=ea7f2ec7419b33064accc22aac5169a6`)
-        .then(function (resp) {return resp.json()})
-        .then(function (data) {
-            document.querySelector('.weather__temp').innerHTML = Math.round(data.main.temp) + ' F';
-            switchStatus = false;
-        })
-        .catch(function () {
-            //Обрабатываем ошибки
-        });
+        console.log(weatherTemp.innerHTML)
+        weatherTemp.innerHTML = Math.round(Number(weatherTemp.innerHTML) + 273);
+        switchStatus = false;
     }
     else if(switchStatus == false) {
-        fetch(`http://api.openweathermap.org/data/2.5/weather?q=${weatherCity.innerHTML}&appid=ea7f2ec7419b33064accc22aac5169a6`)
-        .then(function (resp) {return resp.json()})
-        .then(function (data) {
-            document.querySelector('.weather__temp').innerHTML = Math.round(data.main.temp - 273) + ' C&deg;';
-            switchStatus = true;
-        })
-        .catch(function () {
-            //Обрабатываем ошибки
-        });
+        console.log(weatherTemp.innerHTML)
+        weatherTemp.innerHTML = Math.round(Number(weatherTemp.innerHTML) - 273);
+        switchStatus = true;
     }
 }
